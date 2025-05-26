c916ce1817f3f808e3119b0c91ed3cf60f46f88a
Amund <amundhalgunset@gmail.comm>
2025-05-26 14:11:00 +0200

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    historyApiFallback: true, // Enables deep link routing in dev
+  },
+});
